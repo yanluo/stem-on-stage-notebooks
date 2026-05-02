41b5fcff4edd71131bacdb9f7301bab5c117d524
Yan Luo <yanluo.uml@gmail.com>
2026-05-02 08:19:59 -0400

    L11: fix wearable_datalogger.py to use MakeCode datalogger.* API
    MakeCode rejected the previous log.set_columns/log.log/log.delete calls
    with "unknown object type; cannot lookup attribute 'set_columns'".
    Switch to the real MakeCode v3 datalogger API: set_column_titles,
    log(create_cv(...), ...), delete_log(DeleteType.FAST).

--- L11/wearable_datalogger.py
+++ L11/wearable_datalogger.py
@@ -1,5 +1,6 @@
 # Wearable datalogger for micro:bit v2 (MakeCode Python).
-# Paste into https://python.microbit.org/ or MakeCode's Python view, then flash.
+# Paste into https://makecode.microbit.org/ -> Python view, then flash.
+# (Datalogger requires micro:bit v2.)
 #
 # Buttons:
 #   A -> clear flash log and start recording (check icon)
@@ -8,7 +9,7 @@
 # After recording: plug the board into a laptop, open MICROBIT/MY_DATA.HTM,
 # click Download to save as microbit-log-<name>.csv.
 
-log.set_columns(["t", "x", "y", "z"])
+datalogger.set_column_titles("t", "x", "y", "z")
 
 recording = False
 t0 = 0
@@ -16,10 +17,10 @@ t0 = 0
 
 def on_button_pressed_a():
     global recording, t0
-    log.delete(full=True)               # clear previous run
+    datalogger.delete_log(datalogger.DeleteType.FAST)   # clear previous run
     t0 = input.running_time()
     recording = True
-    basic.show_icon(IconNames.YES)      # check = recording
+    basic.show_icon(IconNames.YES)                      # check = recording
 
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
@@ -27,19 +28,19 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 def on_button_pressed_b():
     global recording
     recording = False
-    basic.show_icon(IconNames.NO)       # X = stopped
+    basic.show_icon(IconNames.NO)                       # X = stopped
 
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 
 def on_forever():
     if recording:
-        log.log({
-            "t": (input.running_time() - t0) / 1000,    # seconds since A pressed
-            "x": input.acceleration(Dimension.X),
-            "y": input.acceleration(Dimension.Y),
-            "z": input.acceleration(Dimension.Z),
-        })
-    basic.pause(50)                     # ~20 Hz
+        datalogger.log(
+            datalogger.create_cv("t", (input.running_time() - t0) / 1000),
+            datalogger.create_cv("x", input.acceleration(Dimension.X)),
+            datalogger.create_cv("y", input.acceleration(Dimension.Y)),
+            datalogger.create_cv("z", input.acceleration(Dimension.Z)),
+        )
+    basic.pause(50)                                     # ~20 Hz
 
 basic.forever(on_forever)
